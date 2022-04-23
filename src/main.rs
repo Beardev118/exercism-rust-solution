3faf1279971fea9a8f1f4319975c3c34630b5292
GC <otoku118@aol.com>
2022-04-23 13:04:57 +0300

    difference of squares

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 pub mod amstrongNumbers;
 pub mod anagram;
 pub mod beerSong;
+pub mod differenceOfSquares;
 pub mod series;
 
 use amstrongNumbers::is_armstrong_number;
 use anagram::anagrams_for;
 use beerSong::sing;
+use differenceOfSquares::{square_of_sum, sum_of_squares};
 use series::series;
 
 fn main() {
@@ -20,6 +22,9 @@ fn main() {
     println!("{:?}", is_armstrong_number(153));
 
     println!("{:?}", anagrams_for(word, &anagrams));
+
+    println!("{:?}", square_of_sum(10));
+    println!("{:?}", sum_of_squares(10));
 
     // let s: &str = "Hello world!".as_ref();
     // let char_vec: Vec<char> = s.chars().collect();
