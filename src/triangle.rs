3fde2c1a511f72d34bb004dc19c5d5952277757b
GC <otoku118@aol.com>
2022-05-03 20:27:28 +0300

    Triangle

---
+++ src/triangle.rs
@@ -0,0 +1,29 @@
+use std::ops::{Add, Sub};
+
+pub struct Triangle<T>([T; 3]);
+
+impl<T> Triangle<T>
+where
+    T: Default + Copy + PartialEq + PartialOrd + Add<Output = T> + Sub<Output = T>,
+{
+    pub fn build(sides: [T; 3]) -> Option<Triangle<T>> {
+        if sides.iter().any(|x| *x == T::default()) {
+            return None;
+        }
+        let mut sorted_sides = sides;
+        sorted_sides.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        if sorted_sides[2] - (sorted_sides[0] + sorted_sides[1]) > T::default() {
+            return None;
+        }
+        Some(Triangle(sorted_sides))
+    }
+    pub fn is_equilateral(&self) -> bool {
+        self.0[0] == self.0[1] && self.0[1] == self.0[2]
+    }
+    pub fn is_scalene(&self) -> bool {
+        self.0[0] != self.0[1] && self.0[1] != self.0[2] && self.0[0] != self.0[2]
+    }
+    pub fn is_isosceles(&self) -> bool {
+        self.0[0] == self.0[1] || self.0[1] == self.0[2] || self.0[0] == self.0[2]
+    }
+}
