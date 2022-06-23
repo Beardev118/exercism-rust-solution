0b2fdc6d947554365e518a713e81f25d32d2cc68
GC <otoku118@gmail.com>
2022-06-23 12:37:56 +0200

    luhn

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-pub mod macros;
+pub mod luhn;
+use luhn::is_valid;
 
 fn main() {
     println!("Hello, world!");
 
-    let _empty: ::std::collections::HashMap<(), ()> = hashmap!();
-    let _without_comma = hashmap!(23=> 623, 34 => 21);
-    let _with_trailing = hashmap!(23 => 623, 34 => 21,);
+    is_valid("112 33a");
+    println!("{}", is_valid("059"));
 }
