de1818bd0fbc89ba9b806fd2bb6d06712086f01f
GC <otoku118@gmail.com>
2022-06-24 02:08:09 +0200

    simple linked list

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-pub mod perfectNumbers;
-use perfectNumbers::classify;
+pub mod simpleLinkedList;
+use simpleLinkedList::SimpleLinkedList;
 
 fn main() {
     println!("Hello, world!");
 
-    let pt = classify(10000000);
-    println!("{:?}", pt);
+    let mut list: SimpleLinkedList<u32> = SimpleLinkedList::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    let mut rev_list = list.rev();
+    assert_eq!(rev_list.pop(), Some(1));
+    assert_eq!(rev_list.pop(), Some(2));
+    assert_eq!(rev_list.pop(), Some(3));
+    assert_eq!(rev_list.pop(), None);
 }
---
+++ src/simpleLinkedList.rs
@@ -0,0 +1,77 @@
+use std::iter::FromIterator;
+
+pub struct SimpleLinkedList<T> {
+    head: Option<Box<Node<T>>>,
+}
+
+struct Node<T> {
+    data: T,
+    next: Option<Box<Node<T>>>,
+}
+
+impl<T> SimpleLinkedList<T> {
+    pub fn new() -> Self {
+        SimpleLinkedList { head: None }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.head.is_none()
+    }
+
+    pub fn len(&self) -> usize {
+        let mut current_node = &self.head;
+        let mut cnt = 0;
+        while let Some(x) = current_node {
+            cnt += 1;
+            current_node = &x.next;
+        }
+        cnt
+    }
+
+    pub fn push(&mut self, _element: T) {
+        self.head = Some(Box::new(Node {
+            data: _element,
+            next: self.head.take(),
+        }));
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.data
+        })
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.data)
+    }
+
+    #[must_use]
+    pub fn rev(mut self) -> SimpleLinkedList<T> {
+        let mut ret = SimpleLinkedList::new();
+        while let Some(x) = self.pop() {
+            ret.push(x);
+        }
+        ret
+    }
+}
+
+impl<T> FromIterator<T> for SimpleLinkedList<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(_iter: I) -> Self {
+        let mut list = SimpleLinkedList::new();
+        for element in _iter {
+            list.push(element);
+        }
+        list
+    }
+}
+
+impl<T> From<SimpleLinkedList<T>> for Vec<T> {
+    fn from(mut _linked_list: SimpleLinkedList<T>) -> Vec<T> {
+        let mut vec = Vec::new();
+        while !_linked_list.is_empty() {
+            vec.insert(0, _linked_list.pop().unwrap());
+        }
+        vec
+    }
+}
